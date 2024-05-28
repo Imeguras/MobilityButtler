bd8b75dc7d8583883a4507457cad281e5850472b
Ivo Afonso Bispo <ivoafobispo@gmail.com>
2024-05-28 19:47:20 +0100

    Text To Speech Startup

--- app/src/main/java/com/ipleiria/anaivojoao/mobilitybuttler/MainActivity.kt
+++ app/src/main/java/com/ipleiria/anaivojoao/mobilitybuttler/MainActivity.kt
@@ -39,6 +39,9 @@ class MainActivity : AppCompatActivity() {
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        // Start TextToSpeech TTS
+        val tts = TextToSpeech(this);
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
