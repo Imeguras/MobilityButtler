813ed14082bb81619aeda58c7e87aba3698effbb
Imeguras <imeguras@cock.li>
2024-06-06 14:53:10 +0100

    Get Works

---
+++ app/src/main/java/com/ipleiria/anaivojoao/mobilitybuttler/data/entity/SayTriggers.kt
@@ -0,0 +1,12 @@
+package com.ipleiria.anaivojoao.mobilitybuttler.data.entity
+
+enum class SayTriggers(val keyWords: Set<String>) {
+    TEMPERATURE(setOf("temperature"));
+    companion object {
+        fun getAllCommands() = values().flatMap { set -> set.keyWords.map { it } }
+    }
+}
+
+fun SayTriggers.isIn(text: String): Boolean =
+    keyWords.sumOf { if (text.contains(it)) 1.toInt() else 0 } > 0
+
